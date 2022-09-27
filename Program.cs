3f2cbbddaa80747423adf4fd9e35de59dcbeaf4d
ysnblt <106676214+ysnblt@users.noreply.github.com>
2022-09-27 22:36:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodlar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan alınan sayının karesini alıp , eğer 25 ten büyükse büyüktür ,
+            // küçükse küçüktür yazdıran program
+
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine()); 
+            int kareDeger = kareAl(sayi);
+            if(kareDeger < 25)
+            {
+
+                Console.WriteLine("Sayı 25'ten küçüktür.");
+
+            }
+            else
+            {
+
+                Console.WriteLine("Sayı 25'ten büyüktür");
+
+            }
+        }
+
+        static int kareAl(int a)
+        {
+
+            return a * a;
+
+        }
+    }
+}
